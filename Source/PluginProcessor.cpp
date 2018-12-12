52c83fad6bf1fd3f7f5d2a5cb36cbd7e00c182d2
Bjørn Erik Pedersen <bjorn.erik.pedersen@gmail.com>
2018-12-12 10:02:34 +0100

    Add standalone OSX output (for development)
    And fix deprecation warning.

--- Source/PluginProcessor.cpp
+++ Source/PluginProcessor.cpp
@@ -491,7 +491,7 @@ const String ObxdAudioProcessor::getParameterName (int index)
 	case FREL:
 		return S("FilterRelease");
 	}
-	return String::empty;
+	return String();
 }
 
 const String ObxdAudioProcessor::getParameterText (int index)
